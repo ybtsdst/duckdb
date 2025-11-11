a665566e344e074ee31d6e001704389f433d5b9c
bingtao.yin <bingtao.yin@transwarp.io>
2025-11-11 16:46:09 +0800

    update ide files

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function main {
+  # need to reload vscode to enable cmake language server
+
+  echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+
+  mkdir -p $HOME/.config/ccache
+  echo "cache_dir = /opt/ccache" >> $HOME/.config/ccache/ccache.conf
+  echo "max_size = 20.0G" >> $HOME/.config/ccache/ccache.conf
+
+  # replace container settings.json with our project settings.json
+  pushd $HOME/.vscode-server/data/Machine
+  rm -rf settings.json
+  ln -s /opt/transwarp/duckdb/.devcontainer/settings.json settings.json
+  popd
+}
+
+main $@
